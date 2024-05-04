c195e54e6967d362cb88eba02e0ba208b1021646
LinYu-Ch <linyuenhong@gmail.com>
2024-05-04 16:47:04 -0500

    created weapon view model and set up equipping logic

---
+++ src/main/java/mu/edu/c/models/CreateWeaponViewModel.java
@@ -0,0 +1,64 @@
+package mu.edu.c.models;
+
+import mu.edu.c.entities.Entity;
+import mu.edu.c.entities.Player;
+import mu.edu.c.logger.CharacterLoggerSingleton;
+import mu.edu.c.weapons.IWeapon;
+import mu.edu.c.weapons.WeaponFactoryMethod;
+import mu.edu.c.weapons.WeaponType;
+
+public class CreateWeaponViewModel {
+	private String weaponName;
+	private int weaponTypeIndex;
+	private int weaponSimpleDamage;
+	private int weaponSpecialDamage;
+	private float weaponScaler;
+	private Boolean loggerIsPopulated = true;
+	
+	// checks if the character logger is empty
+	private CharacterLoggerSingleton logger = CharacterLoggerSingleton.getInstance();
+	private Player currentPlayer = logger.readCharacterData();
+	private WeaponFactoryMethod weaponFactoryWeapon = new WeaponFactoryMethod();
+		
+	/**
+	 * @return returns false if character data is empty, true if operation was a success
+	 * */
+	public Boolean equipCharacterWithWeapon() {
+		if (currentPlayer != null) {
+			//gets weapon type enum from the weapon index returned by view
+			WeaponType weaponType = WeaponType.values()[weaponTypeIndex];
+			// create a new weapon
+			IWeapon newWeapon = weaponFactoryWeapon.createWeapon(weaponType, weaponName, weaponSimpleDamage, weaponSpecialDamage, weaponScaler);
+			// adds weapon to current player
+			currentPlayer.setWeaponStrategy(newWeapon);
+			//saves character data
+			logger.logCharacterData(currentPlayer);
+			// returns a flag indicating that the operation was a success
+			return loggerIsPopulated;
+		}
+		// returns a flag indicating that the character page is empty and that a character has to be created
+		loggerIsPopulated = false;
+		return loggerIsPopulated;
+	}
+	
+	public void setWeaponTypeIndex(int weaponTypeIndex) {
+		this.weaponTypeIndex = weaponTypeIndex;
+	}
+
+	public void setWeaponSimpleDamage(int weaponSimpleDamage) {
+		this.weaponSimpleDamage = weaponSimpleDamage;
+	}
+
+	public void setWeaponSpecialDamage(int weaponSpecialDamage) {
+		this.weaponSpecialDamage = weaponSpecialDamage;
+	}
+
+	public void setWeaponScaler(float weaponScaler) {
+		this.weaponScaler = weaponScaler;
+	}
+
+	public void setWeaponName(String weaponName) {
+		this.weaponName = weaponName;
+	}
+
+}
